a5e3b4cd2daf290feae7b7623c56393fa8f87f7c
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-03 10:49:31 +0000

    C24619i4:19:31 PMe3

--- Q1.cpp
+++ Q1.cpp
@@ -34,7 +34,7 @@ int main(){
     cin>>length>>breadth;
 
     rectangle val1, val2;
-    val1 = rectangle (length,10);
+    val1 = rectangle (5,10);
     val2 = rectangle (length);
 
     val1.area();
