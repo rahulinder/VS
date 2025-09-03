8238b9e75e68620f64045c3917a70fa5e79d6567
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-03 10:50:33 +0000

    C24620i4:20:33 PMe3

--- Q1.cpp
+++ Q1.cpp
@@ -33,7 +33,7 @@ int main(){
     // cout<<"Enter length and breadth : ";
     // cin>>length>>breadth;
 
-    rectangle val1(5,10), val2(5);
+    rectangle val1(5,10), val2(5);//im
 
     // val1 = rectangle (5,10);//exlicit
     // val2 = rectangle (length);
