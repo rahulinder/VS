8f5bdb7c5f52cd2571f67af8b4414dcebe019436
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-03 11:05:32 +0000

    C24635i4:35:32 PMe3

--- Q2.cpp
+++ Q2.cpp
@@ -38,8 +38,8 @@ int main(){
 
     rectangle val[3];
     for(int i=0;i<2;i++){
-        val[i] = rectangle(5,10);
-        val[i] = rectangle(5,10);
+        val[i] = rectangle(2,10);
+        val[i] = rectangle(5);
         val[i];
     }
 
