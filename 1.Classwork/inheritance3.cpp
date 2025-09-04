b3e2aa9d7b793b257f08cc4cb0e0b5c276588568
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-04 09:27:56 +0000

    Update comments to include the definition of the 'init' member function in the volume class

--- 1.Classwork/inheritance3.cpp
+++ 1.Classwork/inheritance3.cpp
@@ -1,4 +1,4 @@
 /*define a class area having two data variables l and b in private scope define a member function 
 input to enter the values of l and b. define a function a having integer return type to calculate the
-area, define a class volume having data variable h in private scope
+area, define a class volume having data variable h in private scope, define a member function 'init'
 */
