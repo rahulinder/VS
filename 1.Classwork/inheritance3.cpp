aa54e340912ae31ccacbfee8c8436a0b53e6e655
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-04 09:29:14 +0000

    Add inheritance relationship between class value and class area

--- 1.Classwork/inheritance3.cpp
+++ 1.Classwork/inheritance3.cpp
@@ -2,4 +2,5 @@
 input to enter the values of l and b. define a function a having integer return type to calculate the
 area, define a class volume having data variable h in private scope, define a member function 'init'
 to input the value of h, define a member function v to compute and print the volume.
+class value is inheriting properties form class area.
 */
