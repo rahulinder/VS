74223780bdc47465bfeda7ec3c6a5721607765a9
rahulinder20 <sidhurahulinder@gmail.com>
2025-09-04 09:28:24 +0000

    Update comments to include the definition of a new member function in the class description

--- 1.Classwork/inheritance3.cpp
+++ 1.Classwork/inheritance3.cpp
@@ -1,5 +1,5 @@
 /*define a class area having two data variables l and b in private scope define a member function 
 input to enter the values of l and b. define a function a having integer return type to calculate the
 area, define a class volume having data variable h in private scope, define a member function 'init'
-to input the value of h
+to input the value of h, define a member function v to compute 
 */
